f6b881028b2d48a942263a371943af2e2ca25457
Joao-RCirilo <joao.ricardo1998.jrc@gmail.com>
2022-11-23 09:18:12 -0300

    nicio do script

--- Todo_list/src/js/scripts.js
+++ Todo_list/src/js/scripts.js
@@ -1,5 +1,50 @@
-//seleção de elementos
-const formulario-todo = document.querySelector("#formulario-todo");
-//funções
+  //seleção de elementos
+  const formularioTodo = document.querySelector("#formulario-todo");
+  const inputTodo = document.querySelector("#input-todo");
+  const todoList = document.querySelector("#todo-list");
+  const editForm = document.querySelector("#edit-form");
+  const editTodo = document.querySelector("#edit-input");
+  const btnCancelEdit = document.querySelector("#botao-cancelar-edicao");
+  
+  //funções
+  const saveTodo = (text) => {
+    //cria o template da tarefa
+    //cria a div geral(externa)
+  const todo = document.createElement("div");
+  todo.classList.add("todo");
+  //cria o título
+  const todoTitle = document.createElement("h3");
+  todoTitle.innerText = text;
+  todo.appendChild(todoTitle);
 
-//eventos
+  const doneBtn = document.createElement("button");
+  doneBtn.classList.add("finish-todo");
+  doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>';
+  todo.appendChild(doneBtn);
+  
+  const doneBtn = document.createElement("button");
+  editBtnBtn.classList.add("edit-todo");
+  editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>';
+  todo.appendChild(editBtn);
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.classList.add("remove-todo");
+  deleteBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+  todo.appendChild(deleteBtn);
+  
+  todoList.appendChild(todo);
+  
+  };
+  //eventos
+  formularioTodo.addEventListener("submit", (e) =>{
+    e.preventDefault();
+    
+    
+    //pegar o valor do input pra criar a nova tarefa
+  const inputValue = inputTodo.value;
+  
+  if (inputValue) {
+    //salva o todo
+    saveTodo(inputValue);  
+  }
+  });
